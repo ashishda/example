97e1350206196959412d89b0fa7b8e59caccec0c
ashishda <ashish.1.david@gmail.com>
2012-10-22 19:35:36 +0530

    sab for example initial complete
    Signed-off-by: ashishda <ashish.1.david@gmail.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Example::Application.config.secret_token = '759078dc2abea9eac6bcdb513de1f977b1b18271b4f6e1718d049c1c51ef5f3f04df189403f5ceaead7ef44ae804c25f7ee1c7b496dd21d5536af8a105f0e4d4'
